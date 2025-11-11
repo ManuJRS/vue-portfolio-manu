08b41ffcfc79892e8c90a62709517fffa51615ab
Manu <manueljesusrejonsantana@gmail.com>
2025-11-11 07:36:55 -0600

    feat(ui): :bricks: add main container structure to pages
    Added the primary container layout to all pages to establish a consistent structure

--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -2,7 +2,17 @@
 </script>
 
 <template>
-  <div class="container py-4">
-    <h1 class="mb-3">Información</h1>
-  </div>
+  <b-container fluid>
+    <b-row class="justify-content-center vh-100 my-4">
+      <b-col cols="0" sm="2" class="text-center">
+        <h2>container</h2>
+      </b-col>
+      <b-col cols="12" sm="8" class="text-center">
+        <h2>container</h2>
+      </b-col>
+      <b-col cols="0" sm="2" class="text-center">
+        <h2>container</h2>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,9 +2,19 @@
 </script>
 
 <template>
-  <div class="container py-5 text-center">
-    <h1 class="display-6">404</h1>
+  <b-container fluid>
+    <b-row class="justify-content-center vh-100 my-4">
+      <b-col cols="0" sm="2" class="text-center">
+        <h2>container</h2>
+      </b-col>
+      <b-col cols="12" sm="8" class="text-center">
+            <h1 class="display-6">404</h1>
     <p class="mb-3">Los siento, la página que buscas no existe.</p>
     <RouterLink to="/projects" class="btn btn-primary">Ir a Proyectos</RouterLink>
-  </div>
+      </b-col>
+      <b-col cols="0" sm="2" class="text-center">
+        <h2>container</h2>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
